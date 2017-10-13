0f779c76aa5e4171a02fb505fa30c86c585082ef
Ville <villekarjala@gmail.com>
2017-10-13 20:36:04 +0300

    ui state added to index

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,16 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', ['ui.router']);
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('home', {
+      'url':'/',
+      'templateUrl': 'views/instructions.html'//,
+      //i guess no controller needed as no external data is needed
+      //'controller': ''
+    });
+
+
+  }]);
